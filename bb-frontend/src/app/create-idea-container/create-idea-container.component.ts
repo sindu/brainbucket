d9e194f2534eb935c8eecdac3ae13909ddfe31f8
Hecht <JHecht@it-p.de>
2019-06-14 13:42:06 +0200

    reset idea to create on changing group id

--- bb-frontend/src/app/create-idea-container/create-idea-container.component.ts
+++ bb-frontend/src/app/create-idea-container/create-idea-container.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { Idea } from '../models/model';
 import { IdeasService } from '../services/ideas.service';
 import { Observable } from 'rxjs';
@@ -9,9 +9,10 @@ import { group } from '@angular/animations';
   templateUrl: './create-idea-container.component.html',
   styleUrls: ['./create-idea-container.component.scss']
 })
-export class CreateIdeaContainerComponent implements OnInit {
+export class CreateIdeaContainerComponent implements OnInit, OnChanges {
+
   @Input() groupId: number;
-  idea: Idea = {} as Idea;
+  idea: Idea = this.resetIdea();
   constructor(
     private ideaService: IdeasService
   ) { }
@@ -19,7 +20,14 @@ export class CreateIdeaContainerComponent implements OnInit {
   ngOnInit() {
   }
 
+  ngOnChanges(changes: SimpleChanges): void {
+    this.idea = this.resetIdea();
+  }
+
   createIdea(idea: Idea) {
     this.ideaService.addIdeaToGroup(idea, this.groupId);
+  }
+  private resetIdea(): Idea {
+    return {} as Idea;
   }
 }
