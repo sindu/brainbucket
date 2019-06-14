6d4827bbc1ca70e31b5edcacb1d975f81080868b
Hecht <JHecht@it-p.de>
2019-06-14 13:33:50 +0200

    create-idea-component works

--- bb-frontend/src/app/ideas-list-view/ideas-list-view.component.ts
+++ bb-frontend/src/app/ideas-list-view/ideas-list-view.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
-import { switchMap } from 'rxjs/operators';
+import { switchMap, map } from 'rxjs/operators';
 import { Idea } from '../models/model';
 import { IdeasService } from '../services/ideas.service';
 import { ActivatedRoute } from '@angular/router';
@@ -12,6 +12,7 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class IdeasListViewComponent implements OnInit {
   ideas$: Observable<Idea[]>;
+  groupId$: Observable<number>;
   idea: Idea;
   constructor(
     private ideaService: IdeasService,
@@ -20,6 +21,7 @@ export class IdeasListViewComponent implements OnInit {
 
   ngOnInit() {
     this.ideas$ = this.activatedRoute.params.pipe(switchMap(params => this.ideaService.getIdeasOfGroup(+params.id)));
+    this.groupId$ = this.activatedRoute.params.pipe(map(params => +params.id));
   }
 
   addIdeaToGroup(idea: Idea) {
