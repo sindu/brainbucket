33f65d3cc72b142bed6d6e47fa76eac100f70218
Hecht <JHecht@it-p.de>
2019-06-15 16:32:25 +0200

    layout

--- bb-frontend/src/app/services/user.service.ts
+++ bb-frontend/src/app/services/user.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { LoginDialogComponent } from '../login-dialog/login-dialog.component';
 import { MatDialog } from '@angular/material';
-import { Observable, of } from 'rxjs';
+import { Observable, of, Subject, BehaviorSubject } from 'rxjs';
 import { tap } from 'rxjs/operators';
 
 @Injectable({
@@ -11,16 +11,19 @@ export class UserService {
 
   userProperty = 'bb-user';
   user: string;
+  userSub$ = new BehaviorSubject<string>('user');
+
 
   constructor(public dialog: MatDialog) { }
 
   getUser(): Observable<string> {
-    return of(sessionStorage.getItem(this.userProperty));
+    return this.userSub$;
   }
 
   checkUser(): Observable<string> {
     this.user = sessionStorage.getItem(this.userProperty);
-    return (this.user) ? of(this.user) : this.inputUserDialog();
+    this.userSub$.next(this.user);
+    return (this.user) ? this.userSub$ : this.inputUserDialog();
   }
 
   inputUserDialog(): Observable<string> {
@@ -33,6 +36,7 @@ export class UserService {
       console.log('The dialog was closed: ', result);
       if (result) {
         sessionStorage.setItem(this.userProperty, result);
+        this.userSub$.next(result);
       } else {
         sessionStorage.removeItem(this.userProperty);
       }
